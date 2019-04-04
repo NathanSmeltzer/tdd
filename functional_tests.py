b4522b82c995519f1e7355ef61552c52d6766121
Nathan <nathan.smeltzer@gmail.com>
2019-04-04 12:07:17 -0400

    start new tests.py and function_tests2.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,24 +16,32 @@ class NewVisitorTest(unittest.TestCase):
 
         #she notices the page title header meniions to-do lists
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-#she is inveted to ener a to-do item righ away
+        #she is invited to enter a to-do item righ away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
 
-#hshe types 'buy peacock feathers' int oa text box (her hobby)
+        #she types 'buy peacock feathers' int oa text box (her hobby)
+        inputbox.send_keys('Buy peacock feathers')
 
-#when she hits enter, the page updateds and now th epage lists:
-#"1: buy peakcock feathers" as an item in the tod-do list
+        #when she hits enter, the page updateds and now th epage lists:
+        #"1: buy peakcock feathers" as an item in the tod-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-#there is still atext box invieting her to add another item and she entereds "use peacock feathers to make a fly"
+        #there is still atext box invieting her to add another item and she entereds "use peacock feathers to make a fly"
 
-#the page updates again, and now shows both items on her list
+        #the page updates again, and now shows both items on her list
 
-#dith wonder wheterh the site will mremember her liste. Then she sees the site has gnerated a uqnie url for her - - with some explanatary text
+        #dith wonder wheterh the site will mremember her liste. Then she sees the site has gnerated a uqnie url for her - - with some explanatary text
 
-#she fivitst that url - her to-do list is still there
+        #she fivitst that url - her to-do list is still there
 
-#then she leaves
+        #then she leaves
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')

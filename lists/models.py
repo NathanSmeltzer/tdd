2218464e302a50f6e92fb314550861e613469849
Nate <nathan.smeltzer@gmail.com>
2019-07-22 13:30:55 -0400

    new form for list items

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class List(models.Model):
     id = models.AutoField(primary_key=True, max_length=7)
     description = models.TextField(default='')
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
     text = models.TextField(default='')
